01af616d64e82a1d75187c867b08005a8ff3309c
Osman Karaketir <osmankaraketir@gmail.com>
2021-01-17 22:44:33 +0300

    encode and decode working

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"errors"
 	"fmt"
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/qr"
@@ -10,7 +10,10 @@ import (
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
+	"io"
 	"io/ioutil"
+	"math"
+	//"log"
 	"os"
 )
 
@@ -26,90 +29,129 @@ func checkErr(err error) {
 	}
 }
 
-func recognizeFile(path string) {
-	fmt.Printf("recognize file: %v\n", path)
-	imgdata, err := ioutil.ReadFile(path)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-	img, _, err := image.Decode(bytes.NewReader(imgdata))
-	if err != nil {
-		fmt.Printf("image.Decode error: %v\n", err)
-		return
-	}
+func recognizeQR(img image.Image) (string, error) {
 	qrCodes, err := goqr.Recognize(img)
 	if err != nil {
-		fmt.Printf("Recognize failed: %v\n", err)
-		return
+		return "", err
 	}
-	for _, qrCode := range qrCodes {
-		fmt.Printf("qrCode text: %s\n", qrCode.Payload)
+
+	if len(qrCodes) != 1 {
+		return "", errors.New("more then 1")
 	}
-	return
+
+	return string(qrCodes[0].Payload), nil
 }
 
-const COL = 1080
-const ROW = 1080
-
-var videowriter *gocv.VideoWriter
+const WIDTH = 1080
+const HEIGHT = 1080
 
 func main() {
-	writer, err := gocv.VideoWriterFile("outVideo/opencvout.mp4", "H264", 24, COL, ROW, true)
+
+	//converttoQR()
+
+	video, err := gocv.OpenVideoCapture("outVideo/opencvout.mp4")
 	checkErr(err)
-	videowriter = writer
+	defer video.Close()
+	img := gocv.NewMat()
+	defer img.Close()
+	i := 0
+	fmt.Printf("Start reading device: %v\n", video)
 
-	chunks := encodeBase64("test/test.mp4")
-	tot := len(chunks)
-	for i, chunk := range chunks {
-		encodeQr(chunk, fmt.Sprintf("outQR/%010d.jpg", i))
-		fmt.Printf("total: %d, now:%d\n\r", tot, i)
+	file, err := os.Create("test/outfile.jpg")
+	checkErr(err)
+
+	for {
+		if ok := video.Read(&img); !ok {
+			fmt.Printf("Device closed: %v\n", video)
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+
+		img, err := img.ToImage()
+		checkErr(err)
+		res, err := recognizeQR(img)
+		checkErr(err)
+		data, err := base64.StdEncoding.DecodeString(res)
+		checkErr(err)
+		_, err = file.Write(data)
+		checkErr(err)
+		i++
+		fmt.Println(i)
 	}
-	//encodeVideo(tot)
-	videowriter.Close()
+
+	file.Close()
 }
 
-func encodeBase64(filename string) []string {
-	// Open file on disk.
-	f, _ := os.Open(filename)
+func converttoQR() {
+	videowriter, err := gocv.VideoWriterFile("outVideo/opencvout.mp4", "H264", 24, WIDTH, HEIGHT, true)
+	checkErr(err)
 
-	// Read entire JPG into byte slice.
-	reader := bufio.NewReader(f)
-	content, _ := ioutil.ReadAll(reader)
+	outputLOG := make(chan string, 10000)
+	go func() {
+		for {
+			fmt.Print(<-outputLOG)
+		}
+	}()
 
-	// Encode as base64.
-	encoded := base64.StdEncoding.EncodeToString(content)
-	return Chunks(encoded, 1000)
+	fileChunks := make(chan []byte, 1000)
+
+	count := readCunks("test/test.jpg", 500, fileChunks)
+
+	base64Chunks := make(chan string, 1000)
+	go func() {
+		defer close(base64Chunks)
+		for chunk := range fileChunks {
+			base64Chunks <- encodeBase64(chunk)
+		}
+	}()
+
+	images := make(chan image.Image, 1000)
+
+	go func() {
+		defer close(images)
+		for chunk := range base64Chunks {
+			images <- encodeQr(chunk)
+		}
+	}()
+
+	done := make(chan int)
+	go func() {
+		i := 0
+		for image := range images {
+			test, err := recognizeQR(image)
+			checkErr(err)
+			fmt.Println(test)
+			img, err := gocv.ImageToMatRGB(image)
+			checkErr(err)
+			err = videowriter.Write(img)
+			checkErr(err)
+			fmt.Printf("written %d, tot: %d --- ", i, count)
+			fmt.Printf("fileChunks: %d, base64Chunks: %d, images %d\n", len(fileChunks), len(base64Chunks), len(images))
+			i++
+		}
+		videowriter.Close()
+		done <- 1
+	}()
+
+	<-done
 }
 
-func encodeQr(data, filename string) {
+func encodeBase64(data []byte) string {
+	encoded := base64.StdEncoding.EncodeToString(data)
+	return encoded
+}
+
+func encodeQr(data string) image.Image {
 	// Create the barcode
 	qrCode, err := qr.Encode(data, qr.M, qr.Auto)
 	checkErr(err)
 	// Scale the barcode to 200x200 pixels
-	qrCode, err = barcode.Scale(qrCode, 1080, 1080)
+	qrCode, err = barcode.Scale(qrCode, WIDTH, HEIGHT)
 	checkErr(err)
 
-	// create the output file
-	//fileJpeg, err := os.Create(filename)
-	//checkErr(err)
-	//defer fileJpeg.Close()
-	//
-	//// encode the barcode as png
-	//
-	//err = jpeg.Encode(fileJpeg, qrCode,nil)
-	//checkErr(err)
-
-	img, err := gocv.ImageToMatRGB(qrCode)
-	checkErr(err)
-	if img.Empty() {
-		fmt.Printf("Error reading image from: %v\n", filename)
-		return
-	}
-
-	videowriter.Write(img)
-
-	//recognizeFile(filename)
+	return qrCode
 }
 
 func encodeVideo(tot int) {
@@ -129,23 +171,78 @@ func encodeVideo(tot int) {
 }
 
 // https://stackoverflow.com/a/61469854/8328237
-func Chunks(s string, chunkSize int) []string {
-	if chunkSize >= len(s) {
-		return []string{s}
-	}
-	var chunks []string
-	chunk := make([]rune, chunkSize)
-	len := 0
-	for _, r := range s {
-		chunk[len] = r
-		len++
-		if len == chunkSize {
-			chunks = append(chunks, string(chunk))
-			len = 0
+//func Chunks(s string, chunkSize int) []string {
+//	if chunkSize >= len(s) {
+//		return []string{s}
+//	}
+//	var chunks []string
+//	chunk := make([]rune, chunkSize)
+//	len := 0
+//	for _, r := range s {
+//		chunk[len] = r
+//		len++
+//		if len == chunkSize {
+//			chunks = append(chunks, string(chunk))
+//			len = 0
+//		}
+//	}
+//	if len > 0 {
+//		chunks = append(chunks, string(chunk[:len]))
+//	}
+//	return chunks
+//}
+
+//https://zetcode.com/golang/readfile/
+func readCunks(filename string, chunkSize int, chunkChan chan []byte) (count int) {
+	f, err := os.Open(filename)
+	checkErr(err)
+
+	fi, err := f.Stat()
+	checkErr(err)
+
+	filesize := fi.Size()
+
+	d := float64(filesize) / float64(chunkSize)
+
+	count = int(math.Ceil(d))
+
+	go func() {
+
+		defer f.Close()
+		defer close(chunkChan)
+
+		reader := bufio.NewReader(f)
+
+		checker := false
+		var err error
+		n := 0
+		a := 0
+		var buf []byte
+		for {
+			if checker {
+				a, err = reader.Read(buf[n:])
+				n = n + a
+			} else {
+				buf = make([]byte, chunkSize)
+				n, err = reader.Read(buf)
+			}
+
+			if err != nil {
+				if err != io.EOF {
+					checkErr(err)
+				}
+				break
+			}
+			checker = true
+			if n < chunkSize {
+				continue
+			}
+			chunkChan <- buf[:n]
+			checker = false
 		}
-	}
-	if len > 0 {
-		chunks = append(chunks, string(chunk[:len]))
-	}
-	return chunks
+		if checker {
+			chunkChan <- buf[:n]
+		}
+	}()
+	return
 }
